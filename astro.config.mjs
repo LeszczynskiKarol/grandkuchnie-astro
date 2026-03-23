5f470b3393931d3cf3808b5a34e95808d56a1cf0
LeszczynskiKarol <leszczu123@wp.pl>
2026-03-23 10:46:28 +0100

    git push from local

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.grandkuchnie.pl",
-  integrations: [react(), tailwind(), sitemap()],
+  integrations: [react(), tailwind()],
   output: "static",
   build: {
     assets: "_assets",
